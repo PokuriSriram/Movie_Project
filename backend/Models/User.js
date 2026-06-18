f9db8b42983f97a262024cf7c88e4a39f91728d6
PokuriSriram <pokurisriram55@gmail.com>
2026-06-18 20:09:16 +0530

    Created Database

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,5 +1,30 @@
 const mongoose=require('mongoose');
 
-userData={
-    
-}
+userdata=new mongoose.Schema(
+    {
+       fullname:{
+        type:String,
+        required:true
+       },
+       email:{
+        type:String,
+        required:true,
+        unique:true
+       },
+       password:{
+        type:String,
+        required:true,
+       },
+       phoneno:{
+        type:String,
+        required:true,
+        unique:true
+       }
+    },
+       {
+        timestamps:true
+       }
+    );
+
+
+    module.exports=mongoose.model("User",userdata);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Home from './Pages/Home';
